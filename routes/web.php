14847d1be74f079bc1ecaa8d511176ca2d521659
Mathias Hansen <me@codemonkey.io>
2020-04-04 16:14:56 -0400

    feat: Basic landing page + tailwind

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
+});
+
+Route::get('search', function () {
+    return view('search');
 });
