f4ee5ff2c3a202d94ed78163c5bbe561f719bfca
Mathias Hansen <me@codemonkey.io>
2020-04-04 22:47:23 -0400

    fix: Colors

--- resources/js/components/NotificationSubscriptionModal.vue
+++ resources/js/components/NotificationSubscriptionModal.vue
@@ -54,7 +54,7 @@
             <div>
               <div class="mt-2 relative flex items-start">
                 <div class="absolute flex items-center h-5">
-                  <input name="stores[]" value="wegmans" id="store_wegmans" type="checkbox" class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" checked>
+                  <input name="stores[]" value="wegmans" id="store_wegmans" type="checkbox" class="form-checkbox h-4 w-4 text-orange-600 transition duration-150 ease-in-out" checked>
                 </div>
                 <div class="pl-7 text-sm leading-5">
                   <label for="store_wegmans" class="font-medium text-gray-700">Wegmans</label>
@@ -62,7 +62,7 @@
               </div>
               <div class="mt-3 relative flex items-start">
                 <div class="absolute flex items-center h-5">
-                  <input name="stores[]" value="harristeeter" id="store_harristeeter" type="checkbox" class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out">
+                  <input name="stores[]" value="harristeeter" id="store_harristeeter" type="checkbox" class="form-checkbox h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
                 </div>
                 <div class="pl-7 text-sm leading-5">
                   <label for="store_harristeeter" class="font-medium text-gray-700">Harris Teeter</label>
@@ -91,19 +91,19 @@
 
             <div>
               <div class="mt-2 flex items-center">
-                <input id="criteria_anytime" name="form-input criteria_notifications" type="radio" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out" checked>
+                <input id="criteria_anytime" name="form-input criteria_notifications" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out" checked>
                 <label for="criteria_anytime" class="ml-3">
                   <span class="block text-sm leading-5 font-medium text-gray-700">Slot is available at any time</span>
                 </label>
               </div>
               <div class="mt-3 flex items-center">
-                <input id="criteria_soon" name="form-input criteria_notifications" type="radio" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out">
+                <input id="criteria_soon" name="form-input criteria_notifications" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
                 <label for="criteria_soon" class="ml-3">
                   <span class="block text-sm leading-5 font-medium text-gray-700">Slot is available within the next 3 days</span>
                 </label>
               </div>
               <div class="mt-3 flex items-center">
-                <input id="criteria_today" name="form-input criteria_notifications" type="radio" class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out">
+                <input id="criteria_today" name="form-input criteria_notifications" type="radio" class="form-radio h-4 w-4 text-orange-600 transition duration-150 ease-in-out">
                 <label for="criteria_today" class="ml-3">
                   <span class="block text-sm leading-5 font-medium text-gray-700">Slot is available today</span>
                 </label>
@@ -113,7 +113,7 @@
 
           <div class="mt-5 sm:mt-6">
             <span class="flex w-full rounded-md shadow-sm">
-              <button @click="submit" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-blue-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+              <button @click="submit" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-orange-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-orange-500 focus:outline-none focus:border-orange-700 focus:shadow-outline-orange transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                 Notify me
               </button>
             </span>
