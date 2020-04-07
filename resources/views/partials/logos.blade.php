741b278312e92deb7ac400badbe8abd2f3c7e055
Mathias Hansen <me@codemonkey.io>
2020-04-07 17:47:47 -0400

    feat: Added more logos

--- resources/views/partials/logos.blade.php
+++ resources/views/partials/logos.blade.php
@@ -3,12 +3,71 @@
     <p class="text-center text-base leading-6 font-semibold uppercase text-gray-600 tracking-wider">
       Works with
     </p>
-    <div class="mt-6 grid grid-cols-2 gap-0.5 lg:mt-8">
+    <div class="mt-6 grid grid-cols-6 gap-0.5 lg:mt-8">
       <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
         <img class="max-h-12" src="{{ asset('img/logos/wegmans.svg') }}" alt="Wegmans" />
       </div>
       <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
         <img class="max-h-12" src="{{ asset('img/logos/harristeeter.svg') }}" alt="Harris Teeter" />
+      </div>
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/fredmeyer.svg') }}" alt="Fred Meyer" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/qfc.svg') }}" alt="QFC" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/ralphs.svg') }}" alt="Ralphs" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/smiths.svg') }}" alt="Smith's" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/frys.svg') }}" alt="Fry's" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/citymarket.svg') }}" alt="City Market" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/kingsoopers.svg') }}" alt="King Soopers" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/dillons.svg') }}" alt="Dillons" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/kroger.svg') }}" alt="Kroger" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/picknsave.svg') }}" alt="Pick 'n Save" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/metromarket.svg') }}" alt="Metro Market" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/copps.svg') }}" alt="Copps" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/gerbes.svg') }}" alt="Gerbes" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/jaycfoodsstores.svg') }}" alt="JayC Foods Stores" />
+      </div>
+
+      <div class="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+        <img class="max-h-12" src="{{ asset('img/logos/owensmarket.png') }}" alt="Owen's Market" />
       </div>
     </div>
     <p class="text-center text-xs leading-6 text-gray-400">
