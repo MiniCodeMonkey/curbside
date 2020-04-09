ebe8293b8dad0033147a477ce13d830b09f7fe0e
Mathias Hansen <me@codemonkey.io>
2020-04-08 23:15:15 -0400

    feat: Store scanner retries with expontentional backoff

--- app/StoreScanner.php
+++ app/StoreScanner.php
@@ -3,6 +3,13 @@
 namespace App;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ConnectException;
 use Illuminate\Support\Collection;
 
 abstract class StoreScanner
@@ -15,7 +22,31 @@ abstract class StoreScanner
     protected $enableSSLVerification = true;
 
     public function __construct() {
+        $handlerStack = HandlerStack::create(new CurlHandler());
+        $retryMiddleware = Middleware::retry(function ($retries, Request $request, Response $response = null, RequestException $exception = null) {
+            if ($retries >= 3) {
+                return false;
+            }
+
+            if ($exception instanceof ConnectException) {
+                info('Retrying due to ConnectException');
+                return true;
+            }
+
+            if ($response) {
+                if ($response->getStatusCode() >= 500) {
+                    info('Retrying due to status code ' . $response->getStatusCode());
+                    return true;
+                }
+            }
+
+            return false;
+        });
+
+        $handlerStack->push($retryMiddleware);
+
         $this->client = new Client([
+            'handler' => $handlerStack,
             'base_uri' => $this->baseUri,
             'timeout' => 15,
             'cookies' => true,
