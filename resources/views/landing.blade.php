616661c3b637cd70d74cf6f74ff1ef2adc16b189
Mathias Hansen <me@codemonkey.io>
2020-04-09 15:46:46 -0400

    feat: Added banner

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
+<div class="relative bg-orange-600">
+  <div class="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+    <div class="pr-16 sm:text-center sm:px-16">
+      <p class="font-medium text-white">
+        <strong>Sorry!</strong> Kroger and Kroger-owned stores are not working right now. We are working on finding a solution.
+      </p>
+    </div>
+  </div>
+</div>
+
 <landing></landing>
 
 @include('partials.howitworks')
