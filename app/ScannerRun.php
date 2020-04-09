a360a566fd8e3cb6eec340429b27e3c21923e389
Mathias Hansen <me@codemonkey.io>
2020-04-08 21:01:29 -0400

    fix: chain_id should be fillable

--- app/ScannerRun.php
+++ app/ScannerRun.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ScannerRun extends Model
 {
     protected $fillable = [
+        'chain_id',
         'hostname',
         'status',
         'duration_seconds',
