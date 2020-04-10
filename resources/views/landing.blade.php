1784cc7b203346e0c5675141b2deeea9be250fe8
Mathias Hansen <me@codemonkey.io>
2020-04-09 21:33:10 -0400

    feat: Moved and updated announcement

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -2,15 +2,7 @@
 
 @section('content')
 
-<div class="relative bg-orange-600">
-  <div class="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-    <div class="pr-16 sm:text-center sm:px-16">
-      <p class="font-medium text-white">
-        <strong>Sorry!</strong> Kroger and Kroger-owned stores are not working right now. We are working on finding a solution.
-      </p>
-    </div>
-  </div>
-</div>
+@include('partials.announcement')
 
 <landing :chains='@json($chains->map->name)'></landing>
 
---
+++ resources/views/partials/announcement.blade.php
@@ -0,0 +1,9 @@
+<div class="relative bg-orange-600">
+  <div class="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+    <div class="pr-16 sm:text-center sm:px-16">
+      <p class="font-medium text-white">
+        <strong>Sorry!</strong> Kroger and Kroger-owned stores are currently no longer available. We are working on finding a solution.
+      </p>
+    </div>
+  </div>
+</div>
